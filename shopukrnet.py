9c5431aa87981dc5abf3b2a3360d2fc248a58f99
Mishakrons <atomm6666@gmail.com>
2025-11-01 14:57:14 +0200

    Add files via upload

---
+++ shopukrnet.py
@@ -0,0 +1,14 @@
+apples = int(input("How many apples do you want?"))
+bananas = int(input("How many bananas do you want?"))
+orange = int(input("How many oranges do you want?"))
+apple_cost = 17
+banana_cost = 25
+orange_cost = 30
+result_apples = (apple_cost*apples) 
+print("apples cost:", result_apples)
+result_bananas = (banana_cost*bananas)
+print("bananas costs:", result_bananas)
+result_oranges = (orange_cost*orange)
+print("oranges costs:", result_oranges)
+result = result_apples + result_bananas + result_oranges#
+print("all costs:",result)
